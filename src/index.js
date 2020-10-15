1b4bb1467b6f3f768c8e06d5c7df1362972b4d71
sergiipoliakov <64687462+sergiipoliakov@users.noreply.github.com>
2020-10-15 23:42:12 +0200

    dd

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log(555);
+import string from './module-1'
+
+function component() {
+  const element = document.createElement('div');
+
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
+
+
+console.log('dsfsfsfsdfs');
+console.log(string);
---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+    filename: 'my-bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+};
